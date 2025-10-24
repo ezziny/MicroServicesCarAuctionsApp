5067b8476410908f2d61a8719e0a76a999313ff7
ezziny <ezzinynone@gmail.com>
2025-10-24 06:11:24 +0300

    now it's less tightly coupled but it'll still need some work

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,7 @@
+using System.Net;
 using MongoDB.Driver;
-using MongoDB.Entities;
+using MongoDB.Entities;using Polly;
+using Polly.Extensions.Http;
 using SearchService.Entities;
 using SearchService.Extensions;
 using SearchService.Services;
@@ -9,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,13 +25,20 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-try
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbSeeder.SeedAsync(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-    throw;
-}
+    try
+    {
+        await DbSeeder.SeedAsync(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Failed to seed database: " + e.Message);
+        Console.WriteLine("The application will continue running without seeded data.");
+    }
+});
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy() => HttpPolicyExtensions.HandleTransientHttpError()
+    .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+    .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(5));
